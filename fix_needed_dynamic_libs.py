f78f874306576409b0d46a3125c5168d881ec587
luca <luca.penasa@gmail.com>
2016-11-08 14:29:27 +0100

    fix libs

--- fix_needed_dynamic_libs.py
+++ fix_needed_dynamic_libs.py
@@ -1,14 +1,47 @@
 executable = "CloudCompare.exe"
-dll_dir =  "/var/run/media/luca/data/mxe.git/"
+dll_dir =  "/root/mxe.git/"
 
 import fnmatch
 import os
 import subprocess
 import re
-import numpy as np
-
-import os
 import shutil
+
+
+files = [Qt5Concurrent.dll,
+Qt5Core.dll,
+Qt5Gui.dll,
+Qt5OpenGL.dll,
+Qt5PrintSupport.dll,
+Qt5Svg.dll,
+Qt5Widgets.dll,
+libboost_filesystem-mt.dll,
+libboost_regex-mt.dll,
+libboost_system-mt.dll,
+libbz2.dll,
+libfreetype-6.dll,
+libgcc_s_seh-1.dll,
+libglib-2.0-0.dll,
+libgomp-1.dll,
+libharfbuzz-0.dll,
+libiconv-2.dll,
+libintl-8.dll,
+libjasper-1.dll,
+libjpeg-9.dll,
+liblcms2-2.dll,
+liblzma-5.dll,
+libmng-2.dll,
+libpcre-1.dll,
+libpcre16-0.dll,
+libpng16-16.dll,
+libstdc++-6.dll,
+libtiff-5.dll,
+libwebp-5.dll,
+libwebpdemux-1.dll,
+libwinpthread-1.dll,
+zlib1.dll]
+
+
 
 
 def find_file (srcdir, pattern):
@@ -25,62 +58,17 @@ found = find_file("./", executable)[0]
 exedir = os.path.dirname(found)
 
 
-print ("exe is in dir " + str(exedir) )
-#os.chdir(exedir)
 
+for dll in files:
+    print (str(dll))
+    qua = find_file(dll_dir, dll)
 
-def test_run(exefile):
-    print ("launching exe")
-    p = subprocess.Popen(["wine", exefile], stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE,cwd=exedir)
-    #output.kill()
-
-    import time
-    time.sleep(2)
-    p.terminate()
-    output = p.communicate()
-
-    print ("output received: " + str(output[1]))
-
-
-    return str(output[1])
-
-
-def parse_wine_output(output):
-    pattern = 'err:module:import_dll Library\s(.*?.dll) \(which is needed by'
-    qua = re.findall(pattern  , output,   re.DOTALL)
-    return qua
-
-
-done = False
-while (done != True):
-
-    out= test_run(found)
-
-    qua = parse_wine_output(out)
-
-    print (qua)
-
-    dlls = np.unique(np.sort(qua))
-
-
-    print ("needed dlls: " + str(dlls) )
-
-    if len(dlls) == 0:
-        done = True
-        print("no ddls are needed. everything ok!   " )
+    if len(qua) == 0:
+        print("-------> cannot find file " + str(dll))
         continue
 
-
-    for dll in dlls:
-        print (str(dll))
-        qua = find_file(dll_dir, dll)
-
-        if len(qua) == 0:
-            print("-------> cannot find file " + str(dll))
-            continue
-
-        print ("copying " + str(qua[0]))
-        print( "copying in  " + str(exedir))
+    print ("copying " + str(qua[0]))
+    print( "copying in  " + str(exedir))
 
 
-        shutil.copy2    (qua[0], exedir)
+    shutil.copy2    (qua[0], exedir)
