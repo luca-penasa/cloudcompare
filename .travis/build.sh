ed0efd7f73f5080bb1ec4ba1211270619d8e4630
luca <luca.penasa@gmail.com>
2016-11-10 15:27:48 +0100

    newtests

--- .travis/build.sh
+++ .travis/build.sh
@@ -34,6 +34,39 @@ fi
 
 if [ $OS = "LINUX" ]; then
 	echo "compiling for linux"
+	mkdir build
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=Release
+      -DQT5_ROOT_PATH=/opt/qt55
+      -DOPTION_USE_DXF_LIB=ON
+      -DOPTION_USE_SHAPE_LIB=ON
+      -DINSTALL_QANIMATION_PLUGIN=ON
+      -DINSTALL_QBLUR_PLUGIN=ON
+      -DINSTALL_QDUMMY_PLUGIN=ON
+      -DINSTALL_QEDL_PLUGIN=ON
+      -DINSTALL_QFACETS_PLUGIN=ON
+      -DINSTALL_QHPR_PLUGIN=ON
+      -DINSTALL_QPOISSON_RECON_PLUGIN=ON
+      -DINSTALL_QSRA_PLUGIN=ON
+      -DINSTALL_QSSAO_PLUGIN=ON
+      -DINSTALL_QGMMREG_PLUGIN=ON
+      -DINSTALL_QCSF_PLUGIN=ON
+      -DINSTALL_QPHOTOSCAN_IO_PLUGIN=ON
+      -DINSTALL_QM3C2_PLUGIN=ON
+      -DINSTALL_QPCV_PLUGIN=ON ..
+
+      # -DOPTION_USE_GDAL=ON
+      # -DINSTALL_QKINECT_PLUGIN=ON
+      # -DLIBFREENECT_INCLUDE_DIR="/usr/include"
+      # -DLIBFREENECT_LIBRARY_FILE="/usr/lib/x86_64-linux-gnu/libfreenect.so"
+
+      # For now:
+      # - qRansacSD fails on Clang
+      # - no libav/FFMPEG. Version in repos is < 10.7 (no frame.h)
+      # - could not use CGAL from Ubuntu (< 4.3)
+
+
+	make -j2
 fi
 
 
--- .travis/setup.sh
+++ .travis/setup.sh
@@ -19,10 +19,10 @@ if [ $OS = "MXEWIN" ]; then
 	echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" | sudo tee -a /etc/apt/sources.list
 	sudo  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
 	sudo apt-get update
-	sudo apt-get install -y "${mxearchname}-qtbase"
+	sudo apt-get install -y $mxearchname-qtbase
 
 	if [[ $TRAVIS_BRANCH == 'to_vombat' ]]; then # on this branch we also need boost and eigen
-		sudo apt-get install -y "${mxearchname}"-eigen mxe-i686-w64-mingw32.shared-boost
+		sudo apt-get install -y $mxearchname-eigen mxe-i686-w64-mingw32.shared-boost
 	fi
 fi
 
