8867d147b493b4425d43d1cbcee8b0507ae1b528
luca <luca.penasa@gmail.com>
2016-11-10 15:21:49 +0100

    newtests

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,45 +1,39 @@
+#!/bin/bash
+set -e
 echo "compiler ${CXX}, and ${CC}"
 
 
-if [[$OS== "MXEWIN"]; then
+if [ $OS = "MXEWIN" ]; then
 	echo "compiling for win on mxe"
+	echo "travis build dir is ${TRAVIS_BUILD_DIR}"
+	cd ${TRAVIS_BUILD_DIR}
+	mkdir build
+	cd build
 
-	if [[$BITS == 64]]; then
-		echo "compiling for win on mxe 64 bits"		
+	PATH=${PATH}:/usr/lib/mxe/usr/bin:/usr/lib/mxe/tools
+
+	if [ $TRAVIS_BRANCH = 'to_vombat' ]; then 
+		echo "on branch ${TRAVIS_BRANCH}"
+		${mxearchname}-cmake -DCMAKE_BUILD_TYPE=Release .. -DCMAKE_INSTALL_PREFIX=${TRAVIS_BUILD_DIR} -DOPTION_BUILD_CCVIEWER=OFF -DINSTALL_vombat_PLUGIN=ON
+	else # default compile options for all branches
+		${mxearchname}-cmake -DCMAKE_BUILD_TYPE=Release .. -DCMAKE_INSTALL_PREFIX=${TRAVIS_BUILD_DIR} -DOPTION_BUILD_CCVIEWER=OFF	
 	fi
 
-	if [[$BITS == 32]]; then
-		echo "compiling for win on mxe 34 bits"		
-	fi
+
+
+	make install -j 3
+	cd ${TRAVIS_BUILD_DIR}
+	#- wget https://raw.githubusercontent.com/mxe/mxe/master/tools/copydlldeps.sh; chmod a+x copydlldeps.sh
+	copydlldeps.sh -c -F ./CloudCompare -d ./CloudCompare -R /usr/lib/mxe
+	name=cloudcompare-${TRAVIS_BRANCH}-${TRAVIS_COMMIT}
+	mv CloudCompare ${name}
+	tar -zcvf ${name}.tar.gz ${name}
+	curl -X PUT -u ${OWNCLOUD_USER}:${OWNCLOUD_PASSWORD} "https://ns392452.ip-176-31-109.eu/owncloud/remote.php/webdav/cloudcompare-builds/${name}.tar.gz" --data-binary @"${name}.tar.gz" --insecure
 fi
 
 
-if [[$OS == "LINUX"]]; then
+if [ $OS = "LINUX" ]; then
 	echo "compiling for linux"
 fi
 
 
-echo "travis build dir is ${TRAVIS_BUILD_DIR}"
-cd ${TRAVIS_BUILD_DIR}
-mkdir build
-cd build
-
-PATH=${PATH}:/usr/lib/mxe/usr/bin:/usr/lib/mxe/tools
-
-if [[ $TRAVIS_BRANCH == 'to_vombat' ]]; then # on this branch we also need boost and eigen
-	i686-w64-mingw32.shared-cmake -DCMAKE_BUILD_TYPE=Release .. -DCMAKE_INSTALL_PREFIX=${TRAVIS_BUILD_DIR} -DOPTION_BUILD_CCVIEWER=OFF -DINSTALL_vombat_PLUGIN=ON
-else # default compile options for all branches
-
-	i686-w64-mingw32.shared-cmake -DCMAKE_BUILD_TYPE=Release .. -DCMAKE_INSTALL_PREFIX=${TRAVIS_BUILD_DIR} -DOPTION_BUILD_CCVIEWER=OFF	
-fi
-
-
-
-make install -j 3
-cd ${TRAVIS_BUILD_DIR}
-#- wget https://raw.githubusercontent.com/mxe/mxe/master/tools/copydlldeps.sh; chmod a+x copydlldeps.sh
-copydlldeps.sh -c -F ./CloudCompare -d ./CloudCompare -R /usr/lib/mxe
-name=cloudcompare-${TRAVIS_BRANCH}-${TRAVIS_COMMIT}
-mv CloudCompare ${name}
-tar -zcvf ${name}.tar.gz ${name}
-curl -X PUT -u ${OWNCLOUD_USER}:${OWNCLOUD_PASSWORD} "https://ns392452.ip-176-31-109.eu/owncloud/remote.php/webdav/cloudcompare-builds/${name}.tar.gz" --data-binary @"${name}.tar.gz" --insecure
--- .travis/setup.sh
+++ .travis/setup.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-
+set -e
 echo "os ${OS}, compiler ${COMPILER}, bits ${BITS}"
 
 if [ $OS = "MXEWIN" ]; then
+
+
+	if [$BITS == 64]; then
+		mxearchname="x86-64-w64-mingw32.shared"
+		echo "compiling for win on mxe 64 bits"		
+	fi
+
+	if [$BITS == 32]; then
+		mxearchname="i686-w64-mingw32.shared"
+		echo "compiling for win on mxe 34 bits"		
+	fi
+
+
 	echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" | sudo tee -a /etc/apt/sources.list
 	sudo  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
 	sudo apt-get update
-	sudo apt-get install -y mxe-i686-w64-mingw32.shared-qtbase
+	sudo apt-get install -y "${mxearchname}-qtbase"
 
 	if [[ $TRAVIS_BRANCH == 'to_vombat' ]]; then # on this branch we also need boost and eigen
-		sudo apt-get install -y mxe-i686-w64-mingw32.shared-eigen mxe-i686-w64-mingw32.shared-boost
+		sudo apt-get install -y "${mxearchname}"-eigen mxe-i686-w64-mingw32.shared-boost
 	fi
 fi
 
