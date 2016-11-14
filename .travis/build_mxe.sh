5c3e3dc9da1f4bef5097488201ee641c0f63ea4f
luca <luca.penasa@gmail.com>
2016-11-14 12:11:05 +0100

    ci test

---
+++ .travis/build_mxe.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+echo "compiler ${CXX}, and ${CC}"
+
+
+echo "compiling for win on mxe"
+echo "travis build dir is ${TRAVIS_BUILD_DIR}"
+cd ${TRAVIS_BUILD_DIR}
+mkdir build
+cd build
+
+PATH=${PATH}:/usr/lib/mxe/usr/bin:/usr/lib/mxe/tools
+
+if [ $TRAVIS_BRANCH = 'to_vombat' ]; then 
+      echo "on branch ${TRAVIS_BRANCH}"
+      $cmakename -DCMAKE_BUILD_TYPE=Release .. -DCMAKE_INSTALL_PREFIX=${TRAVIS_BUILD_DIR} -DOPTION_BUILD_CCVIEWER=OFF -DINSTALL_vombat_PLUGIN=ON
+else # default compile options for all branches
+      $cmakename -DCMAKE_BUILD_TYPE=Release .. -DCMAKE_INSTALL_PREFIX=${TRAVIS_BUILD_DIR} -DOPTION_BUILD_CCVIEWER=OFF	
+fi
+
+
+
+make install -j 3
+cd ${TRAVIS_BUILD_DIR}
+#- wget https://raw.githubusercontent.com/mxe/mxe/master/tools/copydlldeps.sh; chmod a+x copydlldeps.sh
+copydlldeps.sh -c -F ./CloudCompare -d ./CloudCompare -R /usr/lib/mxe
+name=cloudcompare-${OS}-${BITS}-${TRAVIS_BRANCH}-${TRAVIS_COMMIT}
+mv CloudCompare ${name}
+tar -zcvf ${name}.tar.gz ${name}
+curl -X PUT -u ${OWNCLOUD_USER}:${OWNCLOUD_PASSWORD} "https://ns392452.ip-176-31-109.eu/owncloud/remote.php/webdav/cloudcompare-builds/${name}.tar.gz" --data-binary @"${name}.tar.gz" --insecure
+
---
+++ .travis/build_osx.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+echo "compiler ${CXX}, and ${CC}"
+
+
